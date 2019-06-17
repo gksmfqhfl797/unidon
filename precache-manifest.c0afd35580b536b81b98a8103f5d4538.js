36bb27e0a8f2ba244f9415549194b921e671e1f6
Mink <ahnmink004@gmail.com>
2019-06-18 00:35:45 +0900

    Updates

---
+++ precache-manifest.c0afd35580b536b81b98a8103f5d4538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c494aed9c7caf0d7fde9fc1bf6e5e24",
+    "url": "/unidon/index.html"
+  },
+  {
+    "revision": "f7e81ec5b9531034e376",
+    "url": "/unidon/static/css/main.5ebc370c.chunk.css"
+  },
+  {
+    "revision": "f98c90c4dc4d7aaa0a91",
+    "url": "/unidon/static/js/2.714a5e69.chunk.js"
+  },
+  {
+    "revision": "f7e81ec5b9531034e376",
+    "url": "/unidon/static/js/main.84ebadee.chunk.js"
+  },
+  {
+    "revision": "df9f5ca2a2805248749c",
+    "url": "/unidon/static/js/runtime~main.79df0ccd.js"
+  },
+  {
+    "revision": "d5fc7125b6103f84c9380ed4de7f068f",
+    "url": "/unidon/static/media/unicef.d5fc7125.jpg"
+  }
+]);
